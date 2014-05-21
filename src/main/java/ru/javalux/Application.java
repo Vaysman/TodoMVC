cfb53bcee6f3c0caf9855637e4dc8709abe2d2f6
Mikhail Vaysman <mikhail.vaysman@gmail.com>
2014-05-21 20:56:18 +0400

    rendering jsp from controller

--- src/main/java/ru/javalux/Application.java
+++ src/main/java/ru/javalux/Application.java
@@ -2,13 +2,21 @@ package ru.javalux;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
     }
--- src/main/java/ru/javalux/web/Example.java
+++ src/main/java/ru/javalux/web/Example.java
@@ -1,12 +1,22 @@
 package ru.javalux.web;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.util.Date;
+
+@Controller
 public class Example {
+    @Value("${application.message:Hello World}")
+    private String message = "Hello, World!";
+
     @RequestMapping("/")
-    String home() {
-        return "Hello World!";
+    String home(ModelMap model) {
+        model.put("time", new Date());
+        model.put("message", this.message);
+        return "welcome";
     }
 }
